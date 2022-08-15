60ed17eac3c148e36eb2f4305338498077f39252
abner <fantasrulai@gmail.com>
2022-08-16 01:39:29 +0800

    env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+console.log(process.env)
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/starknetapp',
-  assetPrefix: "/starknetapp/",
+  basePath: process.env.BasePath,
+  assetPrefix: process.env.AssetPrefix,
 
   //https://github.com/vercel/next.js/issues/21079
   images: {
